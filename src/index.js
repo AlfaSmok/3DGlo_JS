b12272cd92c9b67111c558d16bb277718a241a7e
Alfir Zagidullin <alfir1991@gmail.com>
2022-08-02 00:38:29 +0300

    start project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import moduleOne from './modules/one';
+
+moduleOne();
---
+++ src/modules/one.js
@@ -0,0 +1,10 @@
+'use strick';
+
+const oneModule = () => {
+  const one = 'first module';
+
+  console.log(one);
+  console.log('Hello');
+};
+
+export default oneModule;
